aad4510b7dfe38cdc2c6f9c10274c99343127951
Viktor <viktor@nospam.org>
2023-08-16 08:50:10 +0200

    .

---
+++ 3_Objektorientierung/2_Aggregation/Cabrio.cpp
@@ -0,0 +1,13 @@
+#include "Cabrio.h"
+
+#include <iostream>
+
+void Cabrio::oeffnen()
+{
+	std::cout << "Verdeck wird geoeffnet" << std::endl;
+}
+
+void Cabrio::schliessen()
+{
+	std::cout << "Verdeck wird geschlossen" << std::endl;
+}
---
+++ 3_Objektorientierung/2_Aggregation/Cabrio.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Auto.h"
+class Cabrio :
+    public Auto
+{
+public:
+
+    void oeffnen();
+    void schliessen();
+};
+
