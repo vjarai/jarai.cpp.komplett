1fa0ebde09a903f63c1a947b467c566a8c5d3c93
Viktor <viktor@nospam.org>
2024-08-16 08:25:25 +0200

    .

--- 6_Standard_Template_Library/5_regular_expressions/main.cpp
+++ 6_Standard_Template_Library/5_regular_expressions/main.cpp
@@ -7,14 +7,14 @@ int main()
     std::string subject("This is a test");
 
     try {
-        std::regex re("\\w+");
-        std::sregex_iterator next(subject.begin(), subject.end(), re);
+        std::regex regex("\\w+");
+        std::sregex_iterator iterator(subject.begin(), subject.end(), regex);
         std::sregex_iterator end;
 
-        while (next != end) {
-            std::smatch match = *next;
+        while (iterator != end) {
+            std::smatch match = *iterator;
             std::cout << match.str() << "\n";
-            ++next;
+            ++iterator;
         }
     }
     catch (std::regex_error& e) // Syntax error in the regular expression
