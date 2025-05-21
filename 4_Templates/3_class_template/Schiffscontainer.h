d31f62fed1c9c0fd853217adcb5c6d686b2b32e4
test <test@test.de>
2025-05-21 11:36:29 +0200

    .

--- 4_Templates/3_class_template/Schiffscontainer.h
+++ 4_Templates/3_class_template/Schiffscontainer.h
@@ -1,20 +1,32 @@
 #pragma once
 
 
-template <typename T> class Schiffscontainer
+template <typename T>
+// Template-Klasse Schiffscontainer Header Only Template (inline)
+class Schiffscontainer
 {
 private:
     T inhalt_;
 
 public:
-    void beladen(T neuerInhalt)
-    {
-        inhalt_ = neuerInhalt;
-    }
+    void beladen(T neuerInhalt);
 
-    T entladen()
-    {
-        return inhalt_;
-    }
+    T entladen();
 
 };
+
+
+template <typename T>
+void Schiffscontainer<T>::beladen(T neuerInhalt)
+{
+    inhalt_ = neuerInhalt;
+}
+
+
+template <typename T>
+T Schiffscontainer<T>::entladen()
+{
+    return inhalt_;
+}
+
+
