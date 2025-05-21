f281f1c8456dec4705eb95abc942468c835e818e
Viktor <vjarai@users.noreply.github.com>
2025-05-21 18:59:59 +0200

    .

--- 6_Standard_Template_Library/2b_weak_ptr/2b_weak_ptr.cpp
+++ 6_Standard_Template_Library/2b_weak_ptr/2b_weak_ptr.cpp
@@ -5,9 +5,8 @@ class Person
 {
 public:
 
-	std::shared_ptr<Person> _partner;
-
-	//std::weak_ptr<Person> _partner;
+	std::shared_ptr<Person> _partner;		// Memory Leak, wenn circulare referenz!
+	std::weak_ptr<Person> _weak_partner;	// weak_ptr verhindert memory Leak
 
 	Person()
 	{
@@ -19,11 +18,6 @@ public:
 		std::cout << "Person gestorben" << std::endl;
 	}
 
-	void heiraten(std::shared_ptr<Person> neuerPartner)
-	{
-		this->_partner = neuerPartner;
-		neuerPartner->_partner = std::shared_ptr<Person>(this);
-	}
 };
 
 int main()
@@ -31,7 +25,13 @@ int main()
 	auto person1 = std::make_shared<Person>();
 	auto person2 = std::make_shared<Person>();
 
+	// Beide heiraten/kennen sich gegenseitig 
 	person1->_partner = person2;
-	person2->_partner = person1;
+
+	// cirular reference = > memeory leak = > use weak_ptr!
+	// person2->_partner = person1;
+
+	person2->_weak_partner = person1;
+	
 }
 
