047411a897c2c02ca96fbd58f3943593b237ec1c
test <test@test.de>
2025-05-23 10:25:30 +0200

    .

--- 6_Standard_Template_Library/8_fstream/main.cpp
+++ 6_Standard_Template_Library/8_fstream/main.cpp
@@ -4,7 +4,7 @@
 
 void read_file(const std::string& filename, double& value)
 {
-	std::ifstream ifstream(filename, std::ios::binary | std::ios::_Nocreate);
+	std::ifstream ifstream(filename, std::ios::binary);
 
 	if (!ifstream.good())
 		throw std::runtime_error("Kann Datei nicht oeffnen.");
