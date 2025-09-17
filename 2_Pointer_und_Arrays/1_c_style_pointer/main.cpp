3e00728be0af4020cda349b971ea916d2419ae4e
Viktor <vjarai@users.noreply.github.com>
2025-09-17 08:37:09 +0200

    .

--- 2_Pointer_und_Arrays/1_c_style_pointer/main.cpp
+++ 2_Pointer_und_Arrays/1_c_style_pointer/main.cpp
@@ -12,22 +12,23 @@ int main()
 
 	std::cout << "x = " << x << std::endl;
 
-	ptr = &y;   // Schritt 2: Pointer "verbiegen" (optional)
-	*ptr = 202; // Schritt 3: Pointer verwenden
+	ptr = &y;   // Schritt 4: Pointer "verbiegen" (optional)
+	*ptr = 202; // Schritt 5: Pointer verwenden
 
 	// Mit einem Pointer kann man also auf unterschiedliche Variablen zugreifen
-	// d.h. ein Pointer ist so etwas ähnliches wie eine "variable Variable" ;-)
+	// d.h. ein Pointer ist so etwas Ã¤hnliches wie eine "variable Variable" ;-)
 
 	std::cout << "y = " << y << std::endl;
 
 	ptr = nullptr;  // Schritt 4: Pointer zeigt jetzt NIRGENWO hin
-	*ptr = 100;     // Verwendung von nullptr führt zu sofortigem Programmabbruch!
+	*ptr = 100;     // Verwendung von nullptr fÃ¼hrt zu sofortigem Programmabbruch!
 
-	int* ptr2 = &x; // Ebenfalls möglich: Pointer anlegen und sofort initialisieren (1+2)
+	int* ptr2 = &x; // Ebenfalls mÃ¶glich: Pointer anlegen und sofort initialisieren (1+2)
 
 	// Ein int kann nur mit roher Gewalt (cast) in einen Pointer umgewandelt werden.
 	ptr2 = (int*)12345;
-	// Ein reinterpret_cast ist ein "bad smell"!
+
+	// Ein reinterpret_cast (bad smell!) macht das gleiche ist aber besser lesbar!
 	ptr2 = reinterpret_cast<int*>(123456);
 }
 
